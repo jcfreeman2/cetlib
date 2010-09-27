58692c7a36dc3ad7560138b6219b4ccab6aa1fea
Walter E Brown <wb@fnal.gov>
2010-09-27 16:35:10 -0500

    Augment exception testing.

--- test/exception_test.cc
+++ test/exception_test.cc
@@ -1,6 +1,7 @@
 #include "cetlib/exception.h"
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 
 using cet::exception;
@@ -17,10 +18,21 @@ void
 int
   main( )
 {
-  exception e("Hello.");
+  ensure( 1, exception("Test").category() == "Test" );
+
+  exception e("Hello");
+  ensure( 2, e.category() == "Hello" );
 
   e << "I greet you." << 16 << std::endl;
   std::cout << e;
+
+  try {
+    throw exception("Bye") << "Nice meeting you" << std::endl;
+  }
+  catch( exception const & e ) {
+    std::string s = e.explain_self();
+    ensure( 3, s.find("Nice meeting you") != std::string::npos );
+  }
 
   return 0;
 
