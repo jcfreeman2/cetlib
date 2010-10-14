63dd765f5cbf5ad3f659e35166d7e30168388b77
Walter E Brown <wb@fnal.gov>
2010-10-14 17:08:03 -0500

    Improved coded_exception testing.

--- test/coded_exception_test.cc
+++ test/coded_exception_test.cc
@@ -1,5 +1,7 @@
 #include "cetlib/coded_exception.h"
 #include <cstdlib>
+#include <map>
+#include <string>
 
 
 using cet::coded_exception;
@@ -13,9 +15,27 @@ void
 }
 
 
+enum E { E1 = 1, E2, E3 };
+typedef  std::map<E,std::string> Emap;
+
+Emap const &
+  mapper()
+{
+  static  Emap  m;
+  if( m.empty() ) m[E1] = "E1"
+                , m[E2] = "E2"
+                , m[E3] = "E3"
+                ;
+
+  return m;
+}
+
+
 int
   main( )
 {
+
+  coded_exception<E,mapper> e(E1);
 
   return 0;
 
