7eb1d36f209a812c3fd73f39df394bd55ba72a80
Marc Paterno <paterno@fnal.gov>
2010-11-10 13:32:14 -0600

    Back out some unwanted tests

--- test/split_test.cc
+++ test/split_test.cc
@@ -36,34 +36,5 @@ int
     ensure( 13, v[1] == "d" );
     ensure( 14, v[2] == "ef" );
   }
-
-  {
-    std::string s(":");
-    std::vector<std::string> v;
-    split(s, ':', std::back_inserter(v));
-    ensure( 21, v.size() == 2 );
-    ensure( 22, v[0].empty() );
-    ensure( 23, v[1].empty() );
-  }
-
-  {
-    std::string s(":a:");
-    std::vector<std::string> v;
-    split(s, ':', std::back_inserter(v));
-    ensure( 31, v.size() ==3 );
-    ensure( 32, v[0].empty() );
-    ensure( 33, v[1] == "a" );
-    ensure( 34, v[2].empty() );
-  }
-
-  {
-    std::string s(":ab");
-    std::vector<std::string> v;
-    split(s, ':', std::back_inserter(v));
-    ensure( 41, v.size() == 2 );
-    ensure( 42, v[0].empty() );
-    ensure( 43, v[1] == "ab" );
-  }
-
   return 0;
 }
