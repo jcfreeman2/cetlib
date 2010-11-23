70b1535214bc7895ef7ab73785130b5a954159ed
Chris Green <greenc@fnal.gov>
2010-11-23 14:51:49 -0600

    Test script must be told where to find its test exec.
    Safer temp directory use.
    
    Fix $$ to $? for status check.

--- test/find_matching_files_test.sh
+++ test/find_matching_files_test.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-TMP_DIR=/tmp/find_match.$$
+EXEC_DIR=${1:-.}
+
+TMP_DIR=`mktemp -d /tmp/find_match.XXXXXXXXXX`
+
+trap "[[ -f \"$TMP_DIR\" ]] && rm -rf \"$TMP_DIR\"" EXIT
+
 T1="libthis_is_one.so libanother_one.so"
 T2="libthis_is_two.so libanother_two.so"
 T3="libthis_is_three.so"
 T_ALL="$T1 $T2 $T3"
 
-rm -rf ${TMP_DIR}
-mkdir ${TMP_DIR}
-
 for name in ${T_ALL}
 do
-  touch ${TMP_DIR}/${name}
+  touch "${TMP_DIR}/${name}"
 done
 
 echo `pwd`
 
-find_matching_files_test "lib.*one.so" ${TMP_DIR}; test $$ -eq 2 || exit 1
-find_matching_files_test "lib.*two.so" ${TMP_DIR}; test $$ -eq 2 || exit 1
-find_matching_files_test "lib.*three.so" ${TMP_DIR}; test $$ -eq 1 || exit 1
-find_matching_files_test "lib.*four.so" ${TMP_DIR}; test $$ -eq 0 || exit 1
+${EXEC_DIR}/find_matching_files_test "lib.*one.so" ${TMP_DIR}; test $? -eq 2 || exit 1
+${EXEC_DIR}/find_matching_files_test "lib.*two.so" ${TMP_DIR}; test $? -eq 2 || exit 1
+${EXEC_DIR}/find_matching_files_test "lib.*three.so" ${TMP_DIR}; test $? -eq 1 || exit 1
+${EXEC_DIR}/find_matching_files_test "lib.*four.so" ${TMP_DIR}; test $? -eq 0 || exit 1
 
 exit 0
