26385ca1e15f6d4ab0a3481ca6a0c93dc28238a7
Walter E Brown <wb@fnal.gov>
2010-11-26 00:03:52 -0600

    Improve test.

--- test/canonical_number_test.cc
+++ test/canonical_number_test.cc
@@ -54,6 +54,8 @@ int
   ensure( 15,   becomes( "12E4", "120000" ) );
   ensure( 16,   becomes( "12E5", "1.2e6" ) );
   ensure( 17,   becomes( "12.000E5", "1.2e6" ) );
+  ensure( 18,   becomes( "-0.0e-0", "0" ) );
+  ensure( 19,   becomes( "+0.0E0", "0" ) );
 
   ensure( 21, ! becomes( "1.2.3", "" ) );
   ensure( 22, ! becomes( "1.2,3", "" ) );
