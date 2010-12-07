fdf08466b0bf5e5e9246c41fda5865d9a6cc755b
Walter E Brown <wb@fnal.gov>
2010-12-07 09:00:38 -0600

    Expose helper functions escape/unescape.

--- cetlib/canonical_string.h
+++ cetlib/canonical_string.h
@@ -8,6 +8,12 @@
 
 namespace cet {
 
+  std::string
+    escape( std::string const & str );
+
+  std::string
+    unescape( std::string const & str );
+
   bool
     canonical_string( std::string const & str
                     , std::string       & result
