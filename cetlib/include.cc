7f999a15a3e56a4a2ede6ea049d38a95b12faccd
Walter E Brown <wb@fnal.gov>
2010-12-10 16:02:07 -0600

    Improve include's testing; fix uncovered flaws.

--- cetlib/include.cc
+++ cetlib/include.cc
@@ -14,14 +14,15 @@ bool
               )
 {
   static std::string const include_lit("#include \"");
+  std::size_t include_sz = include_lit.size();
 
   for( std::string line; std::getline(in, line);  ) {
-    if( line.find(include_lit) ) {
+    if( line.find(include_lit) == 0 ) {
       if( line.end()[-1] != '\"' )
         return false;
-      std::ifstream f( line.erase(0, include_lit.size())
-                           .erase(line.size()-1, 1)
-                           .c_str()
+      std::ifstream f( line.substr( include_sz
+                                  , line.size() - include_sz - 1
+                                  ).c_str()
                      , std::ios_base::in
                      );
       if( ! f || ! include(f, result) )
--- test/include_test.cc
+++ test/include_test.cc
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <fstream>
 
+
 using cet::include;
 
 
@@ -12,11 +13,75 @@ void
     std::exit(which);
 }
 
+char const file_a[] = "./a.txt";
+char const file_b[] = "./b.txt";
+char const file_i[] = "./i.txt";
+char const file_j[] = "./j.txt";
+
+char const contents_a[] = "abcde\n"
+                          "vwxyz\n";
+char const contents_b[] = "12345\n"
+                          "67890\n";
+char const contents_i[] = "begin\n"
+                          "#include \"./a.txt\"\n"
+                          "end\n";
+char const contents_j[] = "begin\n"
+                          "#include \"./a.txt\"\n"
+                          "#include \"./b.txt\"\n"
+                          "end\n";
+
+void
+  write_files( )
+{
+  std::ofstream a(file_a);  a << contents_a;
+  std::ofstream b(file_b);  b << contents_b;
+  std::ofstream i(file_i);  i << contents_i;
+  std::ofstream j(file_j);  j << contents_j;
+}
+
 
 int
   main( )
 {
-  ensure( 1, true );
+  write_files();
+
+  { std::string result;
+
+    std::ifstream a(file_a);
+    ensure( 1, a );
+    result.clear();
+    include(a, result);
+    ensure( 2, result == std::string(contents_a) );
+
+    std::ifstream b(file_b);
+    ensure( 3, b );
+    result.clear();
+    include(b, result);
+    ensure( 4, result == std::string(contents_b) );
+  }
+
+  { std::string result;
+    std::ifstream i(file_i);
+    ensure( 11, i );
+    result.clear();
+    include(i, result);
+    ensure( 12, result == std::string("begin\n")
+                        + std::string(contents_a)
+                        + std::string("end\n")
+          );
+  }
+
+  { std::string result;
+    std::ifstream j(file_j);
+    ensure( 21, j );
+    result.clear();
+    include(j, result);
+    ensure( 12, result == std::string("begin\n")
+                        + std::string(contents_a)
+                        + std::string(contents_b)
+                        + std::string("end\n")
+          );
+  }
 
   return 0;
 
