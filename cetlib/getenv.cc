c3575d6538281bff5f66f9bd797f26bd6e2c8b33
Walter E Brown <wb@fnal.gov>
2010-12-15 13:38:37 -0600

    Extract throwing getenv; provide corresponding test.

---
+++ cetlib/getenv.cc
@@ -0,0 +1,24 @@
+// ======================================================================
+//
+// getenv - obtain value of environment variable; throw if none such
+//
+// ======================================================================
+
+#include "cetlib/getenv.h"
+
+#include "cetlib/exception.h"
+#include <cstdlib>     // getenv
+
+// ======================================================================
+
+std::string
+  cet::getenv( std::string const & name )
+{
+  char const * p = std::getenv(name.c_str());
+  if( p == 0 )
+    throw cet::exception("getenv")
+      << "Can't find an environment variable named \"" << name << '\"';
+  return p;
+}
+
+// ======================================================================
---
+++ test/getenv_test.cc
@@ -0,0 +1,37 @@
+// ======================================================================
+//
+// test getenv
+//
+// ======================================================================
+
+#include "cetlib/getenv.h"
+
+#include "cetlib/exception.h"
+#include <cstdlib>
+#include <string>
+
+
+void
+  ensure( int which, bool claim )
+{
+  if( not claim )
+    std::exit(which);
+}
+
+
+int
+  main( )
+{
+  ensure( 1, cet::getenv("USER").size() > 0 );
+
+  try {
+    std::string s = cet::getenv("XYZZY_PLUGH");
+    ensure( 11, false );
+  }
+  catch( cet::exception const & e ) {
+    ensure( 12, e.category() == "getenv" );
+  }
+
+  return 0;
+
+}  // main()
