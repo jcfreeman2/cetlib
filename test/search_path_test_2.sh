2c37a780bb0027e21b8a54557005adf45dc3509f
Chris Green <greenc@fnal.gov>
2010-12-20 09:40:38 -0600

    Fix minor problem in test driver script.

--- test/search_path_test_2.sh
+++ test/search_path_test_2.sh
@@ -2,9 +2,9 @@
 
 EXEC_DIR=${1:-.}
 
-export TMP_DIR=`mktemp -d /tmp/find_match.XXXXXXXXXX`
+export TMP_DIR=`mktemp -d /tmp/${0##*/}.XXXXXXXXXX`
 
-trap "[[ -f \"$TMP_DIR\" ]] && rm -rf \"$TMP_DIR\"" EXIT
+trap "[[ -d \"$TMP_DIR\" ]] && rm -rf \"$TMP_DIR\"" EXIT
 
 T1="libthis_is_one.so libanother_one.so"
 T2="libthis_is_two.so libanother_two.so"
@@ -23,5 +23,4 @@ ${EXEC_DIR}/search_path_test_2 "lib.*two.so"   TMP_DIR; test $? -eq 2 || exit 2
 ${EXEC_DIR}/search_path_test_2 "lib.*three.so" TMP_DIR; test $? -eq 1 || exit 3
 ${EXEC_DIR}/search_path_test_2 "lib.*four.so"  TMP_DIR; test $? -eq 0 || exit 4
 
-rm -rf $TMP_DIR
 exit 0
