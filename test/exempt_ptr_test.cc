e5d1481f816792e724aa12a4ea42544d4614b4da
Walter E Brown <wb@fnal.gov>
2011-01-27 16:37:24 -0600

    Slightly improve test coverage.

--- test/exempt_ptr_test.cc
+++ test/exempt_ptr_test.cc
@@ -16,14 +16,24 @@ void
 int
   main( )
 {
-  exempt_ptr<int> p( new int(16) );
-  ensure( 1, *p == 16 );
+  {
+    exempt_ptr<int> p;
+    ensure( 11, p == 0 );
+    ensure( 12, 0 == p );
+  }
 
-  exempt_ptr<int> q( p );
-  ensure( 2, *p == *q );
-  ensure( 3, p == q );
+  {
+    exempt_ptr<int> p( new int(16) );
+    ensure( 21, p != 0 );
+    ensure( 22, 0 != p );
+    ensure( 23, *p == 16 );
 
-  p.reset( new int(0) );
-  return *p;
+    exempt_ptr<int> q( p );
+    ensure( 24, p == q );
+    ensure( 25, *p == *q );
+
+    p.reset( new int(0) );
+    return *p;
+  }
 
 }  // main()
