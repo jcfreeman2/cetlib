bf3c886953063d701327ece744e944f7b7f2b830
Marc Paterno <paterno@fnal.gov>
2011-01-28 12:18:58 -0600

    Add more testing of cet::include.

--- test/search_path_test_3.cc
+++ test/search_path_test_3.cc
@@ -1,6 +1,27 @@
 #define BOOST_TEST_MODULE ( search_path_test_3 test )
 #include "boost/test/auto_unit_test.hpp"
 
-BOOST_AUTO_TEST_SUITE ( search_path_test3 )
+#include <sstream>
+#include <fstream>
+#include <string>
 
-  BOOST_AUTO_TEST_CASE( test_ab
+#include "cetlib/exception.h"
+#include "cetlib/include.h"
+
+BOOST_AUTO_TEST_SUITE ( search_path_test_3 )
+
+BOOST_AUTO_TEST_CASE( missing_file_throws ) {
+  std::istringstream is("#include \"/tmp/aaa\"");
+  std::string result;
+
+  BOOST_CHECK_THROW ( cet::include(is, result), cet::exception );
+}
+
+BOOST_AUTO_TEST_CASE( bad_stream_throws ) {
+  std::ifstream is("no file with this name exists");
+  std::string result;
+
+  BOOST_CHECK_THROW ( cet::include(is, result), cet::exception );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
