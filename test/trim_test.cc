3c115e2dceb9b797b7b66eecdc2eb4e5c43e4ce1
Walter E Brown <wb@fnal.gov>
2011-05-17 14:47:04 -0500

    Add string-trimming functions.

---
+++ test/trim_test.cc
@@ -0,0 +1,32 @@
+#define BOOST_TEST_MODULE ( trim test )
+
+#include "boost/test/auto_unit_test.hpp"
+#include "cetlib/trim.h"
+
+using cet::trim_left;
+using cet::trim_right;
+using cet::trim;
+
+using cet::trim_left_copy;
+using cet::trim_right_copy;
+using cet::trim_copy;
+
+using std::string;
+
+BOOST_AUTO_TEST_SUITE( trim_test )
+
+BOOST_AUTO_TEST_CASE( trimmed_test )
+{
+  string s1("Hello.");
+
+  { string s2(s1);  BOOST_CHECK( trim_left(s2) == s1 ); }
+  { string s2(s1);  BOOST_CHECK( trim_right(s2) == s1 ); }
+  { string s2(s1);  BOOST_CHECK( trim(s2) == s1 ); }
+
+  { string s2(s1);  BOOST_CHECK( trim_left_copy(s2) == s1 ); }
+  { string s2(s1);  BOOST_CHECK( trim_right_copy(s2) == s1 ); }
+  { string s2(s1);  BOOST_CHECK( trim_copy(s2) == s1 ); }
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
