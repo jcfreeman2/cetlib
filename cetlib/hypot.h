a8fa51bbc71a36b07df99e98d191ed7d1f0a2ef4
Walter E Brown <wb@fnal.gov>
2011-06-02 13:48:16 -0500

    Add hypot(), checked_hypot(), and unchecked_hypot().

---
+++ cetlib/hypot.h
@@ -0,0 +1,66 @@
+#ifndef CETLIB_HYPOT_H
+#define CETLIB_HYPOT_H
+
+// ======================================================================
+//
+// hypot: checked and unchecked Euclidean planar hypotenuse calculations
+//
+// ======================================================================
+
+#include "cpp0x/cmath"
+#include "cpp0x/type_traits"
+#include <limits>
+#include <utility>
+
+// ----------------------------------------------------------------------
+
+namespace cet {
+  template< class T >
+  typename std::enable_if<std::is_arithmetic<T>::value,T>::type
+    hypot( T x, T y );
+
+  template< class T >
+  typename std::enable_if<std::is_arithmetic<T>::value,T>::type
+    unchecked_hypot( T x, T y );
+
+  template< class T >
+  typename std::enable_if<std::is_arithmetic<T>::value,T>::type
+    checked_hypot( T x, T y );
+}
+
+// ----------------------------------------------------------------------
+// unchecked_hypot<>:
+
+template< class T >
+inline typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+  cet::unchecked_hypot( T x, T y )
+{ return std::hypot(x, y); }
+
+// ----------------------------------------------------------------------
+// checked_hypot<>:
+
+template< class T >
+typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+  cet::checked_hypot( T x, T y )
+{
+
+  if( std::isinf(x) || std::isinf(y) )
+    return std::numeric_limits<T>::infinity();
+  else if( std::isnan(x) || std::isnan(y) )
+    return std::numeric_limits<T>::quiet_NaN();
+  else
+    return unchecked_hypot(x, y);
+
+}  // checked_hypot<>(,)
+
+// ----------------------------------------------------------------------
+// hypot<>:
+
+template< class T >
+inline typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+  cet::hypot( T x, T y )
+{ return checked_hypot(x, y); }
+
+// ======================================================================
+
+#endif  // CETLIB_HYPOT_H
---
+++ test/hypot_test.cc
@@ -0,0 +1,44 @@
+#define BOOST_TEST_MODULE ( hypot test )
+#include "boost/test/auto_unit_test.hpp"
+#include "boost/test/floating_point_comparison.hpp"
+#include <cmath>
+#include <limits>
+
+#include "cetlib/hypot.h"
+
+using cet::hypot;
+
+BOOST_AUTO_TEST_SUITE( hypot_test )
+
+BOOST_AUTO_TEST_CASE( basic_test ) {
+  BOOST_CHECK_EQUAL( hypot(5, 12), 13 );
+  BOOST_CHECK_CLOSE( hypot(.5, 1.2), 1.3, 0.0001 );
+  BOOST_CHECK_CLOSE( hypot(.05L, .12L), .13L, 0.0001L );
+}
+
+BOOST_AUTO_TEST_CASE( nan_test ) {
+  BOOST_CHECK( std::isnan( hypot( std::numeric_limits<double>::quiet_NaN()
+                                , 1.2
+             )           )      );
+  BOOST_CHECK( std::isnan( hypot( 3.1415926
+                                , std::numeric_limits<double>::quiet_NaN()
+             )           )      );
+}
+
+BOOST_AUTO_TEST_CASE( inf_test ) {
+  BOOST_CHECK( std::isinf( hypot( std::numeric_limits<double>::infinity()
+                                , 1.2
+             )           )      );
+  BOOST_CHECK( std::isinf( hypot( 3.1415926
+                                , std::numeric_limits<double>::infinity()
+             )           )      );
+
+  BOOST_CHECK( std::isinf( hypot( -std::numeric_limits<double>::infinity()
+                                , 1.2
+             )           )      );
+  BOOST_CHECK( std::isinf( hypot( 3.1415926
+                                , -std::numeric_limits<double>::infinity()
+             )           )      );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
