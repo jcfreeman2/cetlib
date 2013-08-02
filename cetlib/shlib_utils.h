d4af40ef18aa52bb6a6fb7c7cefea3e9e5c6f77e
Chris Green <greenc@fnal.gov>
2013-08-02 16:26:41 -0500

    Utilities for getting shared lib prefix and suffix.

---
+++ cetlib/shlib_utils.h
@@ -0,0 +1,9 @@
+#ifndef cetlib_shlib_utils_h_in
+#define cetlib_shlib_utils_h_in
+
+#include <string>
+namespace cet {
+  std::string shlib_prefix();
+  std::string shlib_suffix();
+}
+#endif /* cetlib_shlib_utils_h_in */
