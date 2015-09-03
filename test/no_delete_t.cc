7775490cd4c2b711fdd8c9ea134049698222efab
Kyle Knoepfel <knoepfel@fnal.gov>
2015-09-03 13:28:29 -0500

    Provide cet::no_delete policy

---
+++ test/no_delete_t.cc
@@ -0,0 +1,30 @@
+// ======================================================================
+//
+// no_delete test
+//
+// ======================================================================
+
+#define BOOST_TEST_MODULE ( no_delete test )
+#include "boost/test/auto_unit_test.hpp"
+#include "boost/test/test_tools.hpp"
+#include "cetlib/no_delete.h"
+
+#include <memory>
+#include <iostream>
+
+BOOST_AUTO_TEST_SUITE( no_delete_test )
+
+BOOST_AUTO_TEST_CASE( no_delete_t )
+{
+  std::ostringstream oss1, oss2;
+  auto * ossp1 = &oss1;
+  auto * ossp2 = &oss2;
+  {
+    std::shared_ptr<std::ostream> { &oss1, cet::no_delete() };
+    std::unique_ptr<std::ostream,cet::no_delete> { &oss2 };
+  }
+  BOOST_CHECK_EQUAL(ossp1,&oss1);
+  BOOST_CHECK_EQUAL(ossp2,&oss2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
