6db89bb6907b4329e085f086dc4895e566a1086c
Lynn Garren <garren@fnal.gov>
2016-01-26 10:43:29 -0600

    do not inline std::regex

--- cetlib/split_by_regex.h
+++ cetlib/split_by_regex.h
@@ -26,24 +26,13 @@
 //
 // ======================================================================
 
-#include <algorithm>
-#include <regex>
+#include <vector>
+#include <string>
 
 namespace cet {
 
-  inline
   std::vector<std::string> split_by_regex (std::string const& str,
-                                           std::string const& delimSet )
-  {
-    std::vector<std::string> tokens;
-    std::copy( std::sregex_token_iterator(str.begin(),
-                                          str.end(),
-                                          std::regex( delimSet ),
-                                          -1),
-               std::sregex_token_iterator(),
-               std::back_inserter( tokens ) );
-    return tokens;
-  }
+                                           std::string const& delimSet );
 
 }
 
