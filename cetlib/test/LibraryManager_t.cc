1d5460659941faceb14b7e411dc51002bbe1c5fb
Marc Paterno <paterno@fnal.gov>
2016-04-14 21:02:43 -0500

    Remove user-defined destructor

--- cetlib/test/LibraryManager_t.cc
+++ cetlib/test/LibraryManager_t.cc
@@ -16,7 +16,6 @@ using namespace cet;
 struct LibraryManagerTestFixture {
 
   LibraryManagerTestFixture();
-  ~LibraryManagerTestFixture();
 
   LibraryManager lm;
   LibraryManager const & lm_ref;
@@ -26,10 +25,6 @@ LibraryManagerTestFixture::LibraryManagerTestFixture()
   :
   lm("cetlibtest"),
   lm_ref(lm)
-{
-}
-
-LibraryManagerTestFixture::~LibraryManagerTestFixture()
 {
 }
 
