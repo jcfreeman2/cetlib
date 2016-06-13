5e5daa2245c64d2bb0f2d3e42324b0cfea2c9d26
Kyle Knoepfel <knoepfel@fnal.gov>
2016-06-13 13:34:54 -0500

    Fix ODR error.

--- cetlib/column_width.h
+++ cetlib/column_width.h
@@ -7,30 +7,15 @@
 //
 // ======================================================================
 
-#include <algorithm>
 #include <string>
 #include <vector>
 
 namespace cet {
-  std::string::size_type
-    column_width( std::vector<std::string> const & );
+  std::string::size_type column_width(std::vector<std::string> const&);
 }
 
-// ----------------------------------------------------------------------
-
-std::string::size_type
-  cet::column_width( std::vector<std::string> const &  v  )
-{
-  std::string::size_type  maxlen = 0;
-
-  // invariant: maxlen is the length of the longest string in v[0..i)
-  for( std::vector<std::string>::size_type i = 0
-                                         , e = v.size();  i != e;  ++i )
-    maxlen = std::max( maxlen, v[i].size() );
-
-  return maxlen;
-}  // column_width()
-
-// ======================================================================
-
 #endif
+
+// Local variables:
+// mode: c++
+// End:
