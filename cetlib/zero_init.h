f1df91729f425b18cd4587c40130831af919008f
Kyle Knoepfel <knoepfel@fnal.gov>
2017-06-23 08:04:53 -0500

    Cleanup.

--- cetlib/zero_init.h
+++ cetlib/zero_init.h
@@ -11,70 +11,41 @@
 #include <type_traits>
 
 namespace cet {
-  template< class T
-          , class = typename std::enable_if<  std::is_arithmetic<T>::value
-                                           || std::is_pointer   <T>::value
-                                           >::type
-          >
-    class zero_init;
+  template <typename T,
+            typename = std::enable_if_t<std::is_arithmetic<T>::value || std::is_pointer<T>::value>>
+  class zero_init;
 }
 
 // ----------------------------------------------------------------------
 
-template< class T, class >
-  class cet::zero_init
-{
-  typedef  typename std::conditional< std::is_const<T>::value
-                                    , T
-                                    , T &
-                                    >::type
-           T_ref;
+template <typename T, typename>
+class cet::zero_init {
+  using T_ref = std::add_lvalue_reference_t<T>;
 
 public:
-  zero_init( )       : val( static_cast<T>(0) )  { }
-  zero_init( T val ) : val( val               )  { }
 
-  // use compiler-generated copy c'tor, copy assignment, and d'tor
+  zero_init() = default;
+  zero_init(T const val) : val{val} {}
 
-#if defined __GXX_EXPERIMENTAL_CXX0X__  || ( defined __cplusplus && __cplusplus >= 201103L )
-  template< class OT
-          , class = typename std::enable_if< std::is_convertible<OT,T>::value
-                                           >::type
-          >
-    zero_init( zero_init<OT> const & other )
-#else
-  template< class OT >
-    zero_init( zero_init<OT> const & other
-             , typename std::enable_if< std::is_convertible<OT,T>::value
-                                      >::type * = 0
-             )
-#endif
-  : val( static_cast<T>(other) )
-  { }
+  template <typename OT,
+            typename = std::enable_if_t<std::is_convertible<OT,T>::value>>
+  zero_init(zero_init<OT> const& other)
+    : val{static_cast<T>(other)}
+  {}
 
-#if defined __GXX_EXPERIMENTAL_CXX0X__
-  template< class OT
-          , class = typename std::enable_if< std::is_convertible<OT,T>::value
-                                           >::type
-          >
-  zero_init &
-#else
-  template< class OT >
-  typename std::enable_if< std::is_convertible<OT,T>::value
-                         , zero_init &
-                         >::type
-#endif
-    operator = ( zero_init<OT> const & other )
+  template <typename OT,
+            typename = std::enable_if_t<std::is_convertible<OT,T>::value>>
+  zero_init& operator=(zero_init<OT> const& other)
   {
     val = static_cast<T>(other);
     return *this;
   }
 
-  operator T_ref     ( )        { return val; }
-  operator T const & ( ) const  { return val; }
+  operator T_ref    ()       { return val; }
+  operator T const& () const { return val; }
 
 private:
-  T val;
+  T val{};
 
 };  // zero_init<T>
 
