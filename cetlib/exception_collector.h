be4e50907c6747baa4a0aa473e6807854c99e77e
Kyle Knoepfel <knoepfel@fnal.gov>
2018-01-29 10:32:41 -0600

    Replace moved headers with #error.

--- cetlib/exception_collector.h
+++ cetlib/exception_collector.h
@@ -1,10 +1,1 @@
-#ifndef cetlib_exception_collector_h
-#define cetlib_exception_collector_h
-
-#include "cetlib_except/exception_collector.h"
-
-#endif /* cetlib_exception_collector_h */
-
-// Local Variables:
-// mode: c++
-// End:
+#error "cetlib_except/exception_collector.h is now located at cetlib_except/exception_collector.h."
