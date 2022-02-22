85cfc49f35cca68e8a0f54e11994705bdbf7541f
Kyle Knoepfel <knoepfel@fnal.gov>
2022-02-22 12:57:02 -0600

    Remove obsolete files.

--- cetlib/exception_category_matcher.h
+++
@@ -1,3 +0,0 @@
-#pragma GCC warning                                                            \
-  "cetlib/exception_category_matcher.h is now located at cetlib_except/exception_category_matcher.h."
-#include "cetlib_except/exception_category_matcher.h"
