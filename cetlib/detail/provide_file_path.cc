899f389be832a95b64924c3251fec4c3cac1c58a
Kyle Knoepfel <knoepfel@fnal.gov>
2022-02-25 10:32:56 -0600

    Adjust signature of provide_file_path.

--- cetlib/detail/provide_file_path.cc
+++ cetlib/detail/provide_file_path.cc
@@ -2,9 +2,9 @@
 #include "boost/filesystem.hpp"
 
 std::string
-cet::detail::provide_file_path(std::string const& filename)
+cet::detail::provide_file_path(char const* filename)
 {
-  namespace bfs = boost::filesystem;
-  bfs::path const p{filename};
-  return bfs::complete(p).native();
+  assert(filename);
+  boost::filesystem::path const p{filename};
+  return complete(p).native();
 }
--- cetlib/detail/provide_file_path.h
+++ cetlib/detail/provide_file_path.h
@@ -4,7 +4,9 @@
 #include <string>
 
 namespace cet::detail {
-  std::string provide_file_path(std::string const& filename);
+  // This function takes the value of __FILE__ and converts it to an
+  // absolute path.
+  std::string provide_file_path(char const* filename);
 }
 
 #endif /* cetlib_detail_provide_file_path_h */
